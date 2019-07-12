878482fb82491efb9cc90f14a6d953dcd11b30d7
Elise Spain <elisespain@gmail.com>
2019-07-12 16:16:54 -0700

    Header styles

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+
+
+
 
 const Characters = props => {
 console.log(props.character);
 // if(!props.character)return <p>loading</p>
     return(
       <div className="character">
+      <Card color="green">
         <h3>{props.character.name}</h3>
         <h3>{props.character.birth_year}</h3>
-
-
+      </Card>
+      <br/>
       </div>  
     )
 
